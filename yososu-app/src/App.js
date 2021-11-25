405ecaaea39d768c1e3fd79dc6d811bdfc45c957
shyunju7 <shyunju7@naver.com>
2021-11-25 21:58:47 +0900

    Feat getCurrentPosition settings

--- yososu-app/src/App.js
+++ yososu-app/src/App.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import MapComponent from "./components/Map";
+
 function App() {
-  return <div>app</div>;
+  const [position, setPosition] = useState({
+    latitude: 33.450701,
+    longitude: 126.570667,
+  });
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+
+        setPosition(latitude, longitude);
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      <MapComponent userPosition={position} />
+    </div>
+  );
 }
 
 export default App;
