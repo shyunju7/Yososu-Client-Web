c5c8fdc2a0319bd827cf8b41254fb36ebfbab09d
shyunju7 <shyunju7@naver.com>
2021-11-25 21:34:32 +0900

    Add MapComponnet

---
+++ yososu-app/src/components/Map.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 500px;
+  height: 500px;
+`;
+const { kakao } = window;
+
+const MapComponent = ({ userPosition }) => {
+  useEffect(() => {
+    console.log(`position,`, userPosition);
+    const container = document.getElementById("map");
+    const mapOptions = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, mapOptions);
+  }, []);
+  return <Container id="map"></Container>;
+};
+
+export default MapComponent;
