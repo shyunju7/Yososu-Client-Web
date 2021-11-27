4302de131eed7aa8ac6207b942a1870f1187aedc
shyunju7 <shyunju7@naver.com>
2021-11-27 17:36:04 +0900

    Feat search Input

--- yososu-app/src/components/Search.js
+++ yososu-app/src/components/Search.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { CgSearch } from "react-icons/cg";
 const Container = styled.div`
@@ -24,9 +24,19 @@ const SearchInput = styled.input`
   padding: 0.5rem;
   border-radius: 16px;
   background-color: #ffffff;
-  border: 1px solid #d4d4d4;
+  border: 2px solid #d4d4d4;
   margin-top: 1rem;
   font-size: 0.8rem;
+  color: #303030;
+
+  &::placeholder {
+    color: #c4c4c4;
+  }
+
+  &:focus {
+    outline: none;
+    border: 2px solid #0023eb;
+  }
 `;
 
 const SearchButton = styled.button`
@@ -36,16 +46,25 @@ const SearchButton = styled.button`
   width: 1rem;
   height: 1rem;
   top: 50%;
-  right: 2rem;
+  right: 2.2rem;
+  cursor: pointer;
 `;
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const onChangeSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <Container>
       <SearchWrapper>
-        <SearchInput placeholder="지역 이름으로 장소를 검색해보세요!" />
+        <SearchInput
+          placeholder="지역 이름으로 장소를 검색해보세요!"
+          onChange={onChangeSearch}
+          value={searchTerm}
+        />
         <SearchButton>
-          <CgSearch />
+          <CgSearch color="#C4C4C4" />
         </SearchButton>
       </SearchWrapper>
     </Container>
