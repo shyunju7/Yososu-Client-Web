cdb4161b4e4f8808d61f475b2ce91a5c3908ab2e
shyunju7 <shyunju7@naver.com>
2021-11-27 16:46:36 +0900

    Add SearchComponent

---
+++ yososu-app/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 30%;
+  height: 100%;
+  background-color: #ffffff;
+  border-right: 2px solid #dddddd;
+`;
+
+const Search = () => {
+  return <Container>search</Container>;
+};
+
+export default Search;
