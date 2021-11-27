0ae38031728685a5a3c1fb27a6e22a92e4ad566a
shyunju7 <shyunju7@naver.com>
2021-11-27 17:37:06 +0900

    Style resize Height(calc header size)

--- yososu-app/src/components/Header.js
+++ yososu-app/src/components/Header.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
-  height: 70px;
+  height: 50px;
   border-bottom: 2px solid #e5e5e5;
   display: flex;
   align-items: center;
--- yososu-app/src/pages/Main/styles.js
+++ yososu-app/src/pages/Main/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: calc(100vh - 50px);
   display: flex;
 `;
