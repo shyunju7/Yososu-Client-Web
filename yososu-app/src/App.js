eb31c27b8c85d0a9e8b5a90fa74fd0676d7d45aa
shyunju7 <shyunju7@naver.com>
2021-12-09 15:58:44 +0900

    Style apply bootstrap.css

--- yososu-app/src/App.js
+++ yososu-app/src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "../src/pages/Main";
 import GlobalStyle from "./GlobalStyle";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
