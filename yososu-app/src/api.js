d8d554b0806c5f515cd390c003e297e7cf78d629
shyunju7 <shyunju7@naver.com>
2021-12-11 20:55:52 +0900

    Feat getInventoriesInStockOrder

--- yososu-app/src/api.js
+++ yososu-app/src/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "http://3.34.236.99:8080",
 });
+
+export const yososuApi = {
+  getInventoriesInStockOrder: () => api.get("/inventories/stock/", {}),
+};
