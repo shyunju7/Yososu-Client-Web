a1d4eed2752af3eb2e6aaa1b799940e1ce41f4df
shyunju7 <shyunju7@naver.com>
2021-12-12 21:35:25 +0900

    Add Loading image

--- yososu-app/src/Loading.js
+++ yososu-app/src/Loading.js
@@ -1,17 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-
+import loadingImage from "../src/assets/loading_img.gif";
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 const Text = styled.h3`
   font-family: S-CoreDream-4Regular;
-  font-weight: 0.65rem;
+  font-size: 1rem;
 `;
 
 const Loading = () => (
-  <div>
-    <Text aria-label="loading" role="img">
-      Loading...💧
-    </Text>
-  </div>
+  <Container>
+    <img src={loadingImage} alt="loadingImage" width="200px" />
+    <Text>Loading...</Text>
+  </Container>
 );
 
 export default Loading;
