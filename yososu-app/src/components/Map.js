7ded70e6615e2c400dc03abb0e406f3de7541257
shyunju7 <shyunju7@naver.com>
2021-12-13 16:08:12 +0900

    Feat set user current location

--- yososu-app/src/components/Map.js
+++ yososu-app/src/components/Map.js
@@ -10,6 +10,7 @@ import GreenMarker from "../../src/assets/green_marker.png";
 import YellowMarker from "../../src/assets/yellow_marker.png";
 import RedMarker from "../../src/assets/red_marker.png";
 import GrayMarker from "../../src/assets/gray_marker.png";
+import BlueMarker from "../../src/assets/blue_marker.png";
 const Container = styled.div`
   width: 740px;
   margin: 20px;
@@ -42,14 +43,30 @@ const MapComponent = ({ isClickedItem, result }) => {
   }, [isClickedItem, kakaoMap]);
 
   // 마커 설정 및 클러스터
-  const handleSetMarker = useCallback(
-    (colorMarkerMap, result) => {
-      const imageSize = new kakao.maps.Size(35, 35);
+  const handleClusterMarker = useCallback(
+    (markers) => {
       let clusterer = new kakao.maps.MarkerClusterer({
         map: kakaoMap,
         averageCenter: true,
-        minLevel: 10,
+        minLevel: 8,
       });
+      clusterer.addMarkers(markers);
+      kakao.maps.event.addListener(
+        clusterer,
+        "clusterclick",
+        function (cluster) {
+          let level = kakaoMap.getLevel() - 1;
+          kakaoMap.setLevel(level, { anchor: cluster.getCenter() });
+        }
+      );
+    },
+    [kakaoMap]
+  );
+
+  const handleSetMarker = useCallback(
+    (colorMarkerMap, result) => {
+      const imageSize = new kakao.maps.Size(35, 35);
+
       if (result && result.length > 0) {
         const markers = result.map((item) => {
           return new kakao.maps.Marker({
@@ -65,12 +82,46 @@ const MapComponent = ({ isClickedItem, result }) => {
             ),
           });
         });
-
-        clusterer.addMarkers(markers);
+        handleClusterMarker(markers);
       }
     },
-    [kakaoMap]
+    [kakaoMap, handleClusterMarker]
   );
+
+  const handleGetCurrentLocation = (map) => {
+    const imageSize = new kakao.maps.Size(45, 45);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        let lat = position.coords.latitude,
+          lon = position.coords.longitude;
+
+        let locPosition = new kakao.maps.LatLng(lat, lon);
+
+        let marker = new kakao.maps.Marker({
+          map: map,
+          position: locPosition,
+          image: new kakao.maps.MarkerImage(BlueMarker, imageSize),
+        });
+        let message =
+          '<div style="padding:12px; font-size: 12px; border-radius: 12px; border: 1px solid #d4d4d4; background-color : #ffffff;">지금 여기에 있어요!</div>'; // 인포윈도우에 표시될 내용입니다
+
+        let iwContent = message; // 인포윈도우에 표시할 내용
+
+        let customOverlay = new kakao.maps.CustomOverlay({
+          position: locPosition,
+          content: iwContent,
+          xAnchor: 0.3,
+          yAnchor: 0,
+          zIndex: 3,
+        });
+
+        customOverlay.setMap(map);
+
+        map.setCenter(locPosition);
+        map.setLevel(5);
+      });
+    }
+  };
 
   useMemo(() => {
     let colorMarkerMap = new Map();
@@ -85,6 +136,7 @@ const MapComponent = ({ isClickedItem, result }) => {
     };
     const map = new kakao.maps.Map(mapRef.current, mapOptions);
     setKakaoMap(map);
+    handleGetCurrentLocation(map);
   }, [mapRef]);
 
   useEffect(() => {
