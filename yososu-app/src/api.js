dd914d8e71185bb14d3b1a6f446f29a07dba002c
shyunju7 <shyunju7@naver.com>
2021-12-17 21:36:49 +0900

    Fix proxy settings & Rebuild

--- yososu-app/src/api.js
+++ yososu-app/src/api.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import https from "https";
 
-const api = axios.create({
-  baseURL: "http://3.34.236.99:8080/",
+const agent = new https.Agent({
+  rejectUnauthorized: false,
 });
 
+const api = axios.create({
+  baseURL: "https://3.34.236.99:8080/",
+});
+// httpsAgent: agent
 export const yososuApi = {
   getInventoriesInStockOrder: (addr) =>
     api.get(`inventories/stock/?addr=${addr}`, {}),
