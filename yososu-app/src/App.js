5e35c56b9b69f959c9119610e4c315498948cfe1
shyunju7 <shyunju7@naver.com>
2021-12-19 23:36:52 +0900

    Feat set window sizing

--- yososu-app/src/App.js
+++ yososu-app/src/App.js
@@ -3,14 +3,26 @@ import Header from "./components/Header";
 import Main from "../src/pages/Main";
 import GlobalStyle from "./GlobalStyle";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [windowSize, setResizeWindow] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setResizeWindow(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div>
       <GlobalStyle />
       <Header />
-      <Main />
-      <Footer />
+      <Main windowSize={windowSize} />
+      <Footer windowSize={windowSize} />
     </div>
   );
 }
