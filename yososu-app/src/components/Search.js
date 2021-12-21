f1e5cfb3e7c5feaf696b43c60b4b4554aab9f72f
shyunju7 <shyunju7@naver.com>
2021-12-22 00:19:25 +0900

    Style relayout Infowindow

--- yososu-app/src/components/Search.js
+++ yososu-app/src/components/Search.js
@@ -43,26 +43,23 @@ const Title = styled.span`
 `;
 
 const Guide = styled.div`
-  width: 100%;
-  margin-left: 64px;
-  color: #c4c4c4;
+  color: #dddddd;
   font-size: 12px;
-  text-align: start;
-  align-self: center;
   line-height: 18px;
-  margin-top: 12px;
-  position: relative;
+  margin-bottom: 6px;
 `;
 
 const InfoButton = styled.span`
   position: absolute;
-  top: 10px;
+  top: 20px;
+  right: 20px;
   font-family: S-CoreDream-6Bold;
-  right: 56px;
   background-color: #ffffff;
   border: 2px solid #0023eb;
-  height: 18px;
-  width: 18px;
+  height: 16px;
+  width: 16px;
+  font-size: 16px;
+  padding-top: 1px;
   border-radius: 50%;
   text-align: center;
   color: #0023eb;
@@ -110,22 +107,29 @@ const Search = ({
       <Contents>
         <Title>요소수 요기서</Title>는 요소수 판매처 위치와 재고량 등의 정보를
         제공합니다.
-      </Contents>
-      <Guide>
-        * 요소수 요기서에 모든 정보는 환경부의 공공 데이터를 활용하였습니다:){" "}
-        <br />* 실제 재고 현황과 일부 차이가 있을 수 있으니 확인 후,
-        방문바랍니다. <br />
         <InfoButton id="info">
           <span role="img" aria-label="info">
             i
           </span>
         </InfoButton>
-      </Guide>
+      </Contents>
       {isVisible ? (
         <InfoWindow>
-          공개되는 요소수 가격은 기본적으로 벌크 요소수 가격이며, 페트 요소수
-          가격은 표시된 가격과 다를 수 있습니다. 벌크 요소수가 매진 되었을 경우,
-          페트 요소수 가격으로 업데이트 됩니다.{" "}
+          <Guide>
+            * 요소수 요기서 서비스는 환경부의 공공 데이터를 활용하였습니다 :)
+          </Guide>
+          <Guide>
+            * 실제 재고 현황과 일부 차이가 있을 수 있으니 확인 후에
+            방문바랍니다.
+          </Guide>
+          <Guide>
+            * 공개되는 요소수 가격은 기본적으로 벌크 요소수 가격이며, 페트
+            요소수 가격은 표시된 가격과 다를 수 있습니다.
+          </Guide>
+          <Guide>
+            * 벌크 요소수가 매진 되었을 경우, 페트 요소수 가격으로 업데이트
+            됩니다.
+          </Guide>
         </InfoWindow>
       ) : null}
       <SearchWrapper>
