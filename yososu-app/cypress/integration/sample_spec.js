20e532d6a48d0b26db5760f4428508449641a395
shyunju7 <shyunju7@naver.com>
2021-12-28 15:01:22 +0900

    Test cypress App.js test

---
+++ yososu-app/cypress/integration/sample_spec.js
@@ -0,0 +1,12 @@
+// sample_spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("Main Test", () => {
+  it("Click into main", () => {
+    cy.visit("http://yososu-yogi.shop/");
+  });
+});
